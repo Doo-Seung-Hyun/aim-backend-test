a1c1786b652c1146599446473ff346661c324ff1
Doo-Seung-Hyun <doosh17@naver.com>
2025-04-28 21:10:57 +0900

    초기 셋팅

---
+++ src/main/java/com/aim/aimcodingtest/AimCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.aim.aimcodingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimCodingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AimCodingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aim/aimcodingtest/AimCodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aim.aimcodingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimCodingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
